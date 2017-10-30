6f07de550b179144d389947ade77c3d2b4fc701d
Igor de Andrade Farias Poncell <iafp@ug4c20.windows.cin.ufpe.br>
2017-10-30 09:38:21 -0300

    Creating points

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("dada boladao")
+const P_RADIUS = 4;
+const P_COLOR = 'black';
+
+var target;
+var point;
+
+
+stage.on('click', function(clickEvent){
+	target = clickEvent.target;
+
+	x = clickEvent.x;
+	y = clickEvent.y;
+	
+	point = new Circle(x,y, P_RADIUS).fill(P_COLOR).addTo(stage);
+
+});
+
+stage.on('drag', function(dragEvent){
+});
