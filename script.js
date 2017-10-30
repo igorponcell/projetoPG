0ded8e8cc26c0d3307cd79a768cf63e54d577cc2
Igor de Andrade Farias Poncell <iafp@ug4c20.windows.cin.ufpe.br>
2017-10-30 09:46:00 -0300

    Creating points Array

--- script.js
+++ script.js
@@ -3,17 +3,29 @@ const P_COLOR = 'black';
 
 var target;
 var point;
-
+var ctrlCurves = [[] , []];
+var pointCounter = 0;
 
 stage.on('click', function(clickEvent){
+
+	//creating points
 	target = clickEvent.target;
 
 	x = clickEvent.x;
 	y = clickEvent.y;
 	
 	point = new Circle(x,y, P_RADIUS).fill(P_COLOR).addTo(stage);
-
+	
+	pointCounter++;
+	
+	if(pointCounter % 2 == 0) ctrlCurves[1].push(point); 
+	else ctrlCurves[0].push(point);
+	
+	
 });
 
 stage.on('drag', function(dragEvent){
+	//moving points
+
+	
 });
